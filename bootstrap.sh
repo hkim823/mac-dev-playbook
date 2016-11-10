f656fff73a301eaf97293bcf85dc12d2eb5bb513
Han Su Kim <hkim823@gmail.com>
2016-11-10 15:46:59 -0500

    Adding Rich's awesome script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Install Command Line Tools, thanks to https://github.com/rtrouton
+./install_xcode_command_line_tools.sh
+
 #Install ansible
 sudo easy_install pip
 sudo pip install ansible
