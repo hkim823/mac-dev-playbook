aa36f1ee7450609434013c762d2ae1c942d696d7
Han Su Kim <hkim823@gmail.com>
2016-11-15 12:00:22 -0500

    Fixing Command Line Tools one liner

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-#Install Command Line Tools, thanks to https://github.com/rtrouton
-./install_xcode_command_line_tools.sh
+#Install Command Line Tools, thanks to https://github.com/rtrouton and https://github.com/Homebrew/install
+sudo touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+sudo softwareupdate -i "`softwareupdate -l | grep -B 1 -E "Command Line (Developer|Tools)" | awk -F"*" '/^ +\\*/ {print $2}' | sed 's/^ *//' | tail -n1`"
+sudo rm -f /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
 #Install ansible
 sudo easy_install pip
@@ -9,4 +11,4 @@ sudo pip install ansible
 
 #run playbook
 
-ansible-playbook -vvv main.yml --ask-vault-pass
+ansible-playbook -vvv main.yml
