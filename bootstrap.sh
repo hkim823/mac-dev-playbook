0a25106bbbb4cbbf2ac75ec20c32b81a8c845352
Han Su Kim <hkim823@gmail.com>
2016-12-06 12:36:28 -0500

    Whitespace for readability

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 #Install Command Line Tools, thanks to https://github.com/rtrouton and https://github.com/Homebrew/install
+
 sudo touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 sudo softwareupdate -i "`softwareupdate -l | grep -B 1 -E "Command Line (Developer|Tools)" | awk -F"*" '/^ +\\*/ {print $2}' | sed 's/^ *//' | tail -n1`"
 sudo rm -f /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
 #Install ansible
+
 sudo easy_install pip
 sudo pip install ansible
 
